2555533e55bf682d63c15ab6d75ab4dde82c7e4c
lightningslo <saml@gustavus.edu>
2025-02-18 10:11:51 -0600

    First Commit To Initialize Repo
    Redid with Project7 inside the challenge5zip expanded.

---
+++ src/edu/gac/mcs178/gack/domain/Person.java
@@ -0,0 +1,149 @@
+package edu.gac.mcs178.gack.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gac.mcs178.gack.Utility;
+
+public class Person {
+	
+	private String name;
+	private Place place;
+	private List<Thing> possessions;
+	
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	public Place getPlace() { return place; }
+	public List<Thing> getPossessions() { return possessions; }
+
+	public Person(String name, Place place) {
+		super();
+		this.name = name;
+		this.place = place;
+		this.possessions = new ArrayList<Thing>();
+		place.gain(this);
+	}
+	
+	public void say(String text) {
+		Utility.displayMessage("At " + place + ": " + this + " says -- " + text);
+	}
+	
+	public List<Thing> otherThingsAtSamePlace() {
+		List<Thing> otherThingsAtSamePlace = new ArrayList<Thing>();
+		for (Thing thing : place.getContents()) {
+			if (!possessions.contains(thing)) {
+				otherThingsAtSamePlace.add(thing);
+			}
+		}
+		return otherThingsAtSamePlace;
+	}
+	
+	public List<Person> otherPeopleAtSamePlace() {
+		List<Person> otherPeopleAtSamePlace = new ArrayList<Person>();
+		for (Person occupant : place.getOccupants()) {
+			if (!occupant.equals(this)) {
+				otherPeopleAtSamePlace.add(occupant);
+			}
+		}
+		return otherPeopleAtSamePlace;
+	}
+	
+	public void lookAround() {
+		say("I see " + Utility.verbalizeList(otherPeopleAtSamePlace(), "no people") +
+				" and " + Utility.verbalizeList(otherThingsAtSamePlace(), "no objects") +
+				" and can go " + Utility.verbalizeList(place.exits(), "nowhere"));
+	}
+	
+	public void listPossessions() {
+		say("I have " + Utility.verbalizeList(possessions, "nothing"));
+	}
+	
+	public void read(Scroll scroll) {
+		if ((scroll.isOwned()) && (scroll.getOwner().equals(this))) {
+			scroll.beRead();
+		} else {
+			Utility.displayMessage(this + " does not have " + scroll);
+		}
+	}
+	
+	public void haveFit() {
+		say("Yaaaah! I am upset");
+	}
+	
+	public void moveTo(Place newPlace) {
+		Utility.displayMessage(this + " moves from " + place + " to " + newPlace);
+		place.lose(this);
+		newPlace.gain(this);
+		for(Thing possession : possessions) {
+			place.lose(possession);
+			newPlace.gain(possession);
+		}
+		place = newPlace;
+		greet(otherPeopleAtSamePlace());
+	}
+	
+	public void go(String direction) {
+		Place newPlace = place.neighborTowards(direction);
+		if (newPlace == null) {
+			Utility.displayMessage("You cannot go " + direction + " from " + place);
+		} else {
+			moveTo(newPlace);
+		}
+	}
+	
+	public void take(Thing thing) {
+		if (equals(thing.getOwner())) {
+			Utility.displayMessage(this + " already has " + thing);
+		} else {
+			if (thing.isOwned()) {
+				Person owner = thing.getOwner();
+				owner.lose(thing);
+				owner.haveFit();
+			}
+			thing.setOwner(this);
+			possessions.add(thing);
+			say("I take " + thing);
+		}
+	}
+	public void give(Thing thing) {
+        // Thing needs to identify recipient as the owner    
+       // recipient needs to add to a list of things he/she own  
+      // Person say something like “here we go, XXX”. 
+
+     // otherwise print a message that says “you don’t have the thing”
+
+		if (this!=(thing.getOwner())) {// if the thing’s owner is you (meaning you already own it)
+			Utility.displayMessage(thing + " is not owned by " + this);
+		} else {
+			if (thing.isOwned()) {
+				Person user = thing.getOwner();
+				user.lose(thing);
+				user.haveFit();
+			}
+			thing.setOwner(this);
+			possessions.add(thing);
+			say("I take " + thing);
+		}
+	}
+	
+	public void lose(Thing thing) {
+		if (!equals(thing.getOwner())) {
+			Utility.displayMessage(this + " doesn't have " + thing);
+		} else {
+			thing.becomeUnowned();
+			possessions.remove(thing);
+			say("I lose " + thing);
+		}
+	}
+	
+	public void greet(List<Person> people) {
+		if (!people.isEmpty()) {
+			say("Hi " + Utility.verbalizeList(people, "no one")); // "no one" can't happen
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+}
